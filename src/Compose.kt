a395569d9bf4ac1e1a3be65a63d324b35b11b49a
vera <vero971410@gmail.com>
2022-10-09 21:58:49 +0300

    initial commit

---
+++ src/Compose.kt
@@ -0,0 +1,14 @@
+
+/*
+
+val changingList = remember(state.list) { state.list.filter { it.shouldBeRendered } }
+remember(key) - if the key has been changed, the value will be recalculated and recomposed.
+
+val changingList by remember {
+    derivedStateOf {
+        state.list.filter { it.shouldBeRendered }
+    }
+}
+Same result, but it will be recomposed only if result of filtering will be changed
+
+*/
